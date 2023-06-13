9114830c508aeb2ec82052556c15e9caf179b5f8
mokimoki <tomoki20020929@gmail.com>
2023-06-13 15:25:02 +0900

    format document

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
-$("document").ready(function(){
+$("document").ready(function () {
     $('.mypattern').slick({
-      autoplay: true,
-      autoplaySpeed: 2500,
-      speed: 800,
-      dots: false,
-      arrows: true,
-      centerMode: true,
-      adaptiveHeight: true,
-      centerPadding: '25%',
-      prevArrow: '<button class="slide-arrow prev-arrow"></button>',
-      nextArrow: '<button class="slide-arrow next-arrow"></button>',
-      responsive: [
-        {
-            breakpoint: 769,
-            settings: {
-                centerMode: false,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        speed: 800,
+        dots: false,
+        arrows: true,
+        centerMode: true,
+        adaptiveHeight: true,
+        centerPadding: '25%',
+        prevArrow: '<button class="slide-arrow prev-arrow"></button>',
+        nextArrow: '<button class="slide-arrow next-arrow"></button>',
+        responsive: [
+            {
+                breakpoint: 769,
+                settings: {
+                    centerMode: false,
+                },
             },
-        },
-      ],
+        ],
     });
-  });
+});
